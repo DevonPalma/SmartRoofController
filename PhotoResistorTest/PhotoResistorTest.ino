df142c20ab7d3fe4a4a5fdd9b66b770132b68343
Devon Palma <deedpa1112@gmail.com>
2022-03-05 20:43:41 -0700

    Created models and test code for 5xPhotoresistor circuit

---
+++ PhotoResistorTest/PhotoResistorTest.ino
@@ -0,0 +1,22 @@
+#include <math.h>
+
+const int pins[5] = {14, 15, 16, 17, 18};
+
+
+void setup() {
+  // put your setup code here, to run once:
+  for (int i = 0; i < 5; i++) {
+    pinMode(pins[i], INPUT);
+  }
+}
+
+void loop() {
+  Serial.printf("PINS:  ");
+  for (int i = 0; i < 5; i++) {
+    float curPinVal = analogRead(pins[i]);
+    float val = map(curPinVal, 0.0, 1028.0, 0.0, 3.3);
+    Serial.printf("%d: %.2f    ", i, val);
+  }
+  Serial.printf("\n");
+  delay(200);
+}
