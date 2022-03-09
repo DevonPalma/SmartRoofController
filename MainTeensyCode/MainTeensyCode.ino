b42f6ce8867e74e78dac8f8644282b2d30f1ea27
Devon Palma <deedpa1112@gmail.com>
2022-03-09 10:10:42 -0700

    Implemented ObserverPattern

--- MainTeensyCode/MainTeensyCode.ino
+++ MainTeensyCode/MainTeensyCode.ino
@@ -1,5 +1,37 @@
+#include "ObserverPattern.h"
+
+class TestSubject : public Subject<TestSubject> {
+  private:
+    int _value;
+  public:
+    TestSubject() {};
+    ~TestSubject() {};
+    void setValue(int value) {
+      _value = value;
+      notify();
+    }
+    int getValue() {
+      return _value;
+    }
+} A;
+
+
+class TestObserver : public Observer<TestSubject> {
+  public:
+    TestObserver() {}
+    ~TestObserver() {}
+
+    void update(TestSubject *subject) {
+      int val = subject->getValue();
+      Serial.printf("Subjects value = %d", val);
+    }
+} B;
+
 void setup() {
-  
+  Serial.begin(9600);
+  while (!Serial);
+  A.attach(B);
+  A.setValue(3);
 }
 
 void loop() {
